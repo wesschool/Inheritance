e67297f278c03895735a344ee1de4fa5d82bab84
wesschool <lyonsw@wsesu.org>
2017-02-14 21:20:02 -0500

    Update CollegeStudent.cpp

--- CollegeStudent.cpp
+++ CollegeStudent.cpp
@@ -13,6 +13,15 @@ CollegeStudent::~CollegeStudent()
 {
 }
 
+int CollegeStudent::getPhoneNumber()
+{
+	return phoneNumber;
+}
+
+void CollegeStudent::setPhoneNumber(int number)
+{
+	phoneNumber = number;
+}
 
 std::string CollegeStudent::getEmail()
 {
@@ -20,4 +29,25 @@ std::string CollegeStudent::getEmail()
 	std::string first2CharsOfFirstName = getFirstName().substr(0, 2);
 
 	return getLastName() + first2CharsOfFirstName + emailDomain;
+}
+
+Result CollegeStudent::isUserValid()
+{
+	//call students isUserValid() first
+	Result validationResult = Student::isUserValid();
+
+	//check to see if it passed validation checks
+	if (!validationResult.success)
+	{
+		return validationResult;
+	}
+
+	//do our additional validation check
+	if (getPhoneNumber() == 0)
+	{
+		validationResult.success = false;
+		validationResult.message = "No Phone Number Found";
+	}
+
+	return validationResult;
 }
